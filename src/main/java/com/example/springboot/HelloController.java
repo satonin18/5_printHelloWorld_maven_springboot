014adcf12d0fe44bbf54986e0fb06e5b64ca0617
Satonin Vyacheslav <satonin@lanit.ru>
2020-08-13 21:23:35 +0500

    files

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return "Greetings from Spring Boot! from P.S. 5_printHelloWorld_maven_springboot";
 	}
 
 }
